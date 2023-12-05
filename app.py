f6c1792e3ea19ef5cd085fd8143fa446125500fd
EldonJH <101818396+EldonJH@users.noreply.github.com>
2023-12-05 14:36:43 -0600

    admin login
    added and admin login that check passcodes.txt, with color changing flash messages

--- app.py
+++ app.py
@@ -8,7 +8,13 @@ app.config["DEBUG"] = True
 #flash  the secret key to secure sessions
 app.config['SECRET_KEY'] = 'your secret key'
 
-
+def check_login(username, password):
+    with open('passcodes.txt', 'r') as file:
+        for line in file:
+            user, pwd = line.strip().split(', ')
+            if user == username and pwd == password:
+                return True
+    return False
 
 
 # use the app.route() decorator to create a Flask view function called index()
@@ -18,11 +24,22 @@ def index():
     
     return render_template('index.html')
 
+
 @app.route('/admin/', methods=('GET', 'POST'))
 def admin():
+    alert_message = None
     
-
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        if check_login(username, password):
+            flash('Login successful!', 'success')
+        else:
+            flash('Invalid username or password.', 'error')
+    
     return render_template('admin.html')
+
 
 @app.route('/reservations/', methods=('GET', 'POST'))
 def reservations():
